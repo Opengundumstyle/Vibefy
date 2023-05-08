3c6f127bc28b02b3fcf593bdaa66431645102fe3
Zhile Lin <zhlin04@gmail.com>
2023-05-07 20:36:28 -0700

    dispatch

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,10 +1,13 @@
 import { useParams } from "react-router-dom";
+import { useSelector,useDispatch } from "react-redux";
+import { DetailsHeader,Error,Loader,RelatedSongs } from "../components";
 
-
+import { setActiveSong,playPause } from "../redux/features/playerSlice";
 
 const SongDetails = () => {
 
   const {songid} = useParams()
+  const dispatch = useDispatch()
 
 return (
    <div>SongDetails</div>
