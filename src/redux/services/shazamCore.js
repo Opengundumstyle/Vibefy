12ab87321f8e82c8d5bd6d0545a01170a0eb653d
Zhile Lin <zhlin04@gmail.com>
2023-05-08 13:36:45 -0700

    related artist song fixed

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -5,7 +5,7 @@ import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 export const shazamCoreApi = createApi({
        reducerPath:'shazamCoreApi',
        baseQuery:fetchBaseQuery({
-          baseUrl:'https://shazam-core.p.rapidapi.com/v1',
+          baseUrl:'https://shazam-core.p.rapidapi.com/',
           prepareHeaders:(headers)=>{
                headers.set('X-RapidAPI-Key', 'fcb44aa933msh45762cffca718aep1cd7b4jsnef9ac44965a0')
 
@@ -14,10 +14,10 @@ export const shazamCoreApi = createApi({
        }),
 
        endpoints:(builder)=>({
-         getTopCharts:builder.query({query:()=>'/charts/world'}),
-         getSongDetails:builder.query({query:({songid})=>`/tracks/details?track_id=${songid}`}),
-         getSongRelated:builder.query({query:({songid})=>`/tracks/related?track_id=${songid}`}),
-         getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}`}),
+         getTopCharts:builder.query({query:()=>'v1/charts/world'}),
+         getSongDetails:builder.query({query:({songid})=>`v1/tracks/details?track_id=${songid}`}),
+         getSongRelated:builder.query({query:({songid})=>`v1/tracks/related?track_id=${songid}`}),
+         getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}`}),
        })
 })
 
