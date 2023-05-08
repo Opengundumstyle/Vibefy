e3e4595a66986f171a425ed5dd381ddf845b6b50
Zhile Lin <zhlin04@gmail.com>
2023-05-08 13:05:45 -0700

    issue with artist related songs(invalid api key)

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,8 +1,28 @@
+import SongBar from './SongBar'
 
 
-
-const RelatedSongs = () => (
-  <div>Loader</div>
+const RelatedSongs = ({data,isPlaying,activeSong,handlePauseClick,handlePlayClick,artistId}) => (
+    <div 
+      className='flex flex-col '
+    >
+        <h1 className='font-bold text-3xl text-white'>
+            Related Songs:
+        </h1>
+         <div className='mt-6 w-full flex flex-col'>
+              {data?.map((song,i)=>(
+                  <SongBar
+                      key={`${song.key}-${artistId}`}
+                      song={song}
+                      i={i}
+                      artistId={artistId}
+                      isPlaying={isPlaying}
+                      activeSong={activeSong}
+                      handlePlayClick={handlePlayClick}
+                      handlePauseClick={handlePauseClick}
+                  />
+              ))}
+         </div>
+    </div>
 );
 
 export default RelatedSongs;
