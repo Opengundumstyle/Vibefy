06e035277cacbe9efa980445c8fad6cc732337f7
Zhile Lin <zhlin04@gmail.com>
2023-05-08 13:55:06 -0700

    useEffect for country

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { Error,Loader,SongCard } from '../components';
 
-const CountryTracks = () => <div>CountryTracks</div>;
+const AroundYou = () => {
+      
+    const [country,setCountry] = useState('')
+    const [loading,setLoading] = useState(true)
+    const {activeSong,isPlaying} = useSelector((state)=>state.player)
 
-export default CountryTracks;
+    useEffect(()=>{
+
+    },[country])
+
+     return(
+         <div> 
+            
+         </div>
+     )
+ }
+export default AroundYou;
