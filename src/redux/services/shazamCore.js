d47d085fdaf28a182afc1dcb7a8303e913abee91
Zhile Lin <zhlin04@gmail.com>
2023-05-08 22:17:32 -0700

    genrelist

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -18,7 +18,8 @@ export const shazamCoreApi = createApi({
          getSongDetails:builder.query({query:({songid})=>`v1/tracks/details?track_id=${songid}`}),
          getSongRelated:builder.query({query:({songid})=>`v1/tracks/related?track_id=${songid}`}),
          getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}`}),
-         getSongsByCountry:builder.query({query:(countryCode)=>`v1/charts/country?country_code=${countryCode}`})
+         getSongsByCountry:builder.query({query:(countryCode)=>`v1/charts/country?country_code=${countryCode}`}),
+         getSongsByGenre:builder.query({query:(genre)=>`v1/charts/genre-world?genre_code=${genre}`})
        })
 })
 
@@ -28,4 +29,5 @@ export const{
      useGetSongRelatedQuery,
      useGetArtistDetailsQuery,
      useGetSongsByCountryQuery,
+     useGetSongsByGenreQuery,
 } = shazamCoreApi 
